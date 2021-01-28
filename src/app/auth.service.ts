a4549b50baa2b014748048cf919cab6d167cef49
Vagner Reis <vagnereis10@gmail.com>
2021-01-28 05:28:06 -0300

    Adição do auth.service com métodos de salvar novo usuario e realizar login com autenticação - melhorias no form de login

---
+++ src/app/auth.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Usuario } from './login/usuario'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  apiURL: string = environment.apiURLBase + "/api/usuarios"
+  tokenURL: string = environment.apiURLBase + environment.obterTokenURL
+  clientID: string = environment.clientID
+  clientSecret: string = environment.clientSecret
+
+  constructor(private http: HttpClient) { }
+
+  salvar(usuario: Usuario): Observable<any>{
+    return this.http.post<any>(this.apiURL, usuario)
+  }
+
+  tentarLogar(username: string, password: string): Observable<any>{
+    const params = new HttpParams()
+                        .set('username', username)
+                        .set('password', password)
+                        .set('grant_type', 'password')
+    
+    const headers = {
+      'Authorization': 'Basic ' + btoa(`${this.clientID}:${this.clientSecret}`),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    return this.http.post<any>(this.tokenURL, params.toString(), {headers})
+  }
+
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -10,24 +12,57 @@ export class LoginComponent {
 
   username!: string;
   password!: string;
-  loginError!: boolean;
+  errors!: string[];
   cadastrando!: boolean;
+  mensagemSucesso!: string;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private service: AuthService) { }
 
   onSubmit(){
-    console.log(`User: ${this.username}, Pass: ${this.password}`);
-
-    this.router.navigate(['/home']);
+    // console.log(`User: ${this.username}, Pass: ${this.password}`);
+    this.service.tentarLogar(this.username, this.password)
+        .subscribe(response => {
+          console.log(response)
+          this.router.navigate(['/home']);
+        }, errorResponse => {
+          console.log(errorResponse)
+          this.errors = ['Usuário e/ou senha incorreto(s).']
+          this.mensagemSucesso = ""
+        })
   }
 
   preparaCadastrar(event: any){
     event.preventDefault();
     this.cadastrando = true;
+    this.mensagemSucesso = "";
+    this.errors = []
+    this.username = "";
+    this.password = "";
   }
 
   cancelaCadastro(){
     this.cadastrando = false;
+    this.errors = []
+    this.username = "";
+    this.password = "";
+  }
+
+  cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.service.salvar(usuario)
+      .subscribe(response => {
+        this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login."
+        this.cancelaCadastro();
+        this.username = "";
+        this.password = "";
+        this.errors = [];
+      }, errorResponse => {
+        this.errors = errorResponse.error.errors;
+      })
   }
 
 }
